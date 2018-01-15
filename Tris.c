bf8422a436c9d10924b583aa068744a6c8705fcf
Calta1997 <35455487+Calta1997@users.noreply.github.com>
2018-01-15 14:05:23 +0100

    PrimoFile

---
+++ Tris.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+void stampaTab(int tab [3][3]);
+int controllaVincitore(int tab[3][3]);
+
+int main() {
+char giocatore[2]= {'X', 'O'};
+int tab [3][3];
+int x,y;
+int fine=0;
+int giocatoreAttivo=0;
+int cox;
+int coy;
+int c=0;
+// creo spazi vuoti
+for(x=0; x<3; x++) {
+for(y=0; y<3; y++)
+tab[x][y]= ' ';
+}
+//stampaTab(tab);//debug
+while(controllaVincitore(tab)!=2 || c>=9) {
+stampaTab(tab);
+printf("Giocatore %d: immetti coordinate del punto che vuoi marcare \n [Immetti numero di riga e numero di colonna compresa tra 0 e 2] \n", giocatoreAttivo);
+scanf("%d%d", &cox, &coy);
+if(tab[cox][coy]!=' ')
+printf("la casella e\' gia\' marcata, bufu \n");
+else {
+tab[cox][coy]= giocatore[giocatoreAttivo];
+giocatoreAttivo=!giocatoreAttivo;
+c++;
+}
+}
+fine=controllaVincitore(tab);
+switch(fine) {
+case 0:
+printf("Ha vinto il giocatore 1 \n");
+break;
+case 1:
+printf("Ha vinto il giocatore 2 \n");
+break;
+case 2:
+printf("Non ha vinto nessuno \n");
+break;
+}
+system("PAUSE");
+}
+
+void stampaTab(int tab [3][3]) {
+int x,y;
+for(x=0; x<3; x++) {
+for(y=0; y<3; y++)
+printf(" %c %c", tab [x][y], y!=2 ? '|' : ' ');
+printf("%s", x!=2 ? "\n------------\n" : "\n");
+}
+return;
+}
+
+int controllaVincitore(int tab[3][3]) {
+int x,y;
+int vincitore=2;
+for(x=0; x<3; x++) { // controllo ogni colonna
+if (tab[x][0]==tab[x][1] && tab[x][1]==tab[x][2])
+vincitore=tab[x][0];
+}
+for(y=0; y<3; y++) { // controllo ogni riga
+if (tab[0][y]==tab[1][y] && tab[1][y]==tab[2][y])
+vincitore=tab[0][y];
+} 
+//controllo diagonali
+
+
+if (vincitore=='X' || vincitore=='O') {
+if (vincitore=='X')
+vincitore=0;
+else
+vincitore=1;
+}
+return vincitore;
+}
