b9e8bde6beda2439f2b2177b6b27865649bc1b2d
Aleci <aleci@inventati.org>
2018-02-20 15:04:51 +0000

    Inizio dizionario

---
+++ Gnuciverba/Dizionario.h
@@ -0,0 +1,9 @@
+//Aggiungere in qualsiasi ordine le parole nel vettore giusto. Il carattere DEVE essere solo maiuscolo .
+//Questo dizionario comprende parole riguardanti l'informatica, la privacy e il sistema GNU/Linux
+char *TreLettere[]={"GNU","TUX","LUG","MIT","GCC","GPG","APT","NSA","GIT","FSF","EFF","USB"};
+char *QuattroLettere[]={"ARCH","RUST","JAVA","HTML","SUDO","ROOT","SNAP","KALI","WINE"};
+char *CinqueLettere[]={"LINUS","LINUX","UNIX","PRISM","APPLE"};
+char *SeiLettere[]={"DISTRO","FEDORA","DEBIAN","KERNEL","PACMAN","GITHUB","OLIMEX","GENTOO","JULIAN","EDWARD","UBUNTU"};
+char *SetteLettere[]={"RICHARD","MRROBOT","ASSANGE","WINDOWS","SNOWDEN","TEMPORA","PRIVACY","KUBUNTU","XUBUNTU"};
+char *OttoLettere[]={"TORVALDS","STALLMAN","GUARDIAN"};
+char *NoveLettere[]={"RASPBERRY","WIKILEAKS","CANONICAL"};
